c837fa0a6adb48112edd2f583dae21621e8a4eb7
Kata Venkata Sai Krishna <venkata.krishna@mediamelon.com>
2025-08-14 19:49:08 +0530

    handled window and document to avoid crashes

---
+++ index.js
@@ -0,0 +1,20 @@
+'use client';
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "MMAppAnalyticsSDK", {
+  enumerable: true,
+  get: function get() {
+    return _mmAppAnalyticsJsSdkEsmMin.MMAppAnalyticsSDK;
+  }
+});
+Object.defineProperty(exports, "SHAKAPlayerMMSSIntgr", {
+  enumerable: true,
+  get: function get() {
+    return _mmsmartstreaming_shakaplayerMin.SHAKAPlayerMMSSIntgr;
+  }
+});
+var _mmAppAnalyticsJsSdkEsmMin = require("./mm-app-analytics-js-sdk.esm.min.js");
+var _mmsmartstreaming_shakaplayerMin = require("./mmsmartstreaming_shakaplayer.min.js");
